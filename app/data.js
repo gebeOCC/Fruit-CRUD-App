eafa92426c3840d4f5fedd423d2fc8adf02d3ab9
gebeOCC <occ.gebe.barry@gmail.com>
2023-10-13 00:52:53 +0800

    first commit

---
+++ app/data.js
@@ -0,0 +1,9 @@
+// src/data.js
+const dummyData = [
+    { id: 1, name: 'Apple', description: 'A red fruit', color: 'red' },
+    { id: 2, name: 'Banana', description: 'A yellow fruit', color: 'yellow' },
+    { id: 3, name: 'Orange', description: 'An orange fruit', color: 'orange' },
+  ];
+  
+  export default dummyData;
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+'use client';
+/* App.tsx */
+import React, { useState } from 'react';
+import dummyData from './data';
+import { Fruit } from './types';
 
-export default function Home() {
+function App() {
+  const [fruits, setFruits] = useState<Fruit[]>(dummyData);
+  const [newFruit, setNewFruit] = useState<Fruit>({ id: 0, name: '', description: '', color: '' });
+  const [filter, setFilter] = useState<string>('');
+  const [editItemId, setEditItemId] = useState<number | null>(null);
+
+  const handleCreate = (name: string, color: string) => {
+    if (name.trim() === '' || color.trim() === '') {
+      return;
+    }
+
+    if (editItemId !== null) {
+      setFruits((prevFruits) =>
+        prevFruits.map((fruit) =>
+          fruit.id === editItemId ? { ...fruit, name, color } : fruit
+        )
+      );
+      setEditItemId(null);
+    } else {
+      setFruits([...fruits, { ...newFruit, id: fruits.length + 1, name, color }]);
+    }
+
+    setNewFruit({ id: 0, name: '', description: '', color: '' });
+  };
+
+  const handleEdit = (id: number, updatedFruit: Fruit) => {
+    setNewFruit(updatedFruit);
+    setEditItemId(id);
+  };
+
+  const handleDelete = (id: number) => {
+    setFruits((prevFruits) => prevFruits.filter((fruit) => fruit.id !== id));
+  };
+
+  const filteredFruits = fruits.filter((item) =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className='container'>
+    <div className="create">
+      <h1>Fruit CRUD App</h1>
+      <form>
+        <input
+          type="text"
+          value={newFruit.name}
+          onChange={(e) => setNewFruit({ ...newFruit, name: e.target.value })}
+          placeholder="Enter name"
         />
+        <input
+          type="text"
+          value={newFruit.color}
+          onChange={(e) => setNewFruit({ ...newFruit, color: e.target.value })}
+          placeholder="Enter color"
+        />
+        <button
+          type="button"
+          onClick={() => handleCreate(newFruit.name, newFruit.color)}
+          className={editItemId !== null ? 'button-update' : ''}
+        >
+          {editItemId !== null ? 'Update' : 'Create'}
+        </button>
+      </form>
       </div>
+      <div className="App" >
+      <label>Filter by Name:</label>
+      <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Color</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+<tbody>
+  {filteredFruits.map((item) => (
+    <tr key={item.id}>
+      <td>{item.name}</td>
+      <td style={{ color: item.color }}>{item.color}</td>
+      <td>
+        <button className='tButton' onClick={() => handleEdit(item.id, item)}>Edit</button>
+        <button className='tButton' onClick={() => handleDelete(item.id)}>Delete</button>
+      </td>
+    </tr>
+  ))}
+</tbody>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </table>
+    </div>
+    </div>
+  );
 }
+
+export default App;
